9ecd6cfc5c2c977a555d80403bcee7910cc5f31c
Bilal Itani <bilalitani1@gmail.com>
2017-08-12 16:05:14 -0400

    Initial Commit

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("hit " + other.name + "!");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+
+    public float bulletSpeed = 30;
+
+    public float lifeTime = 3;
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Fire();
+        }
+    }
+
+    private void Fire() 
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(),
+                                bulletSpawn.parent.GetComponent<Collider>()); 
+
+        bullet.transform.position = bulletSpawn.position;
+
+        var rot = bullet.transform.rotation.eulerAngles;
+
+        bullet.transform.rotation = Quaternion.Euler(rot.x, transform.eulerAngles.y, rot.z);
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, lifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay) 
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(bullet);
+    }
+}
